f5fab668b7be0bd3eb4e70951dbd26b144eb8a39
Fredy Santos <fredy0113@gmail.com>
2025-05-12 09:39:05 -0400

    Starting Hosted UI integration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import boto3
 import os
 from dotenv import load_dotenv
@@ -12,9 +12,23 @@ app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
 
+def login_required():
+    pass
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("home.html")
+
+
+@app.route("/callback", methods=["GET", "POST"]) #Using for Cognito Hosted UI
+def callback():
+    return redirect(url_for("dashboard"))
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
